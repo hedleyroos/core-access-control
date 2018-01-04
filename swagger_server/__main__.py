68e71c8116312dc28d711d538a843554b8a0ec67
ruands <ruan.desousa@praekeltconsulting.com>
2018-01-04 14:42:28 +0200

    GE-681: Generated code from Swagger Codegen

---
+++ swagger_server/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import connexion
+from .encoder import JSONEncoder
+
+
+if __name__ == '__main__':
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    app.app.json_encoder = JSONEncoder
+    app.add_api('swagger.yaml', arguments={'title': '# The Access Control API  ## Overview The Access Control API is an API exposed to other core components. It uses an API Key in an HTTP header to perform authentication and authorisation.  Most of the API calls facilitates CRUD of the entities defined in the Access Control component. Others calls allows the retrieval of information in a form that is convenient for other components (most notably the Management Layer) to consume. '})
+    app.run(port=8080)
