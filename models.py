96557738c79cc377fd302bccbc495533bd9a14c7
AltusBarry <AltusBarry@users.noreply.github.com>
2018-01-19 13:40:25 +0200

    feature/GE-709: Fixed composite pk, changed strings to varchars, added some more models

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 import datetime
 import os
+import uuid
 
 from flask import Flask
-
 from flask_migrate import Migrate
-
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
 
 from settings import settings
 
@@ -23,7 +23,7 @@ class Domain(DB.Model):
     domain = DB.relationship("Domain", backref="domain", lazy=True)
     id = DB.Column(DB.Integer, primary_key=True)
     parent_id = DB.Column(DB.Integer, DB.ForeignKey("domain.id"), nullable=True)
-    name = DB.Column(DB.String(30), unique=True, index=True)
+    name = DB.Column(DB.VARCHAR(30), unique=True, index=True)
     description = DB.Column(DB.Text)
     created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
     updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
@@ -32,42 +32,10 @@ class Domain(DB.Model):
         return self.name
 
 
-class Site(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    domain_id = DB.Column(DB.Integer, DB.ForeignKey("domain.id"), index=True)
-    description = DB.Column(DB.Text)
-    client_id = DB.Column(DB.Integer, unique=True, index=True)
-    is_active = DB.Column(DB.Boolean(), default=True)
-    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
-    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
-
-    def __repr__(self):
-        return "%s-%s-%s" % (self.client_id, self.is_active, self.domain_id)
-
-
-class DomainRole(DB.Model):
-    __table_args__ = (
-        DB.ForeignKeyConstraint(
-            [domain_id, role_id],
-            [Domain.id, Role.id]
-        ), {}
-    )
-    domain_id = DB.Column(DB.Integer, primary_key=True)
-    role_id = DB.Column(DB.Integer, primary_key=True)
-    grant_implicitly = DB.Column(DB.Boolean(), default=False)
-    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
-    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
-
-    def __repr__(self):
-        return "%s-%s-%s" % (
-            self.domain_id, self.role_id, self.grant_implicitly
-        )
-
-
 class Role(DB.Model):
     role = DB.relationship("Role", backref="role", lazy=True)
     id = DB.Column(DB.Integer, primary_key=True)
-    label = DB.Column(DB.String(30), unique=True, index=True)
+    label = DB.Column(DB.VARCHAR(30), unique=True, index=True)
     description = DB.Column(DB.Text)
     requires_2fa = DB.Column(DB.Boolean())
     created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
@@ -90,10 +58,79 @@ class Permission(DB.Model):
 
 class Resource(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
-    urn = DB.Column(DB.String(100), unique=True, index=True)
+    urn = DB.Column(DB.VARCHAR(100), unique=True, index=True)
     description = DB.Column(DB.Text)
     created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
     updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
 
     def __repr__(self):
         return self.urn
+
+
+class Site(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    domain_id = DB.Column(DB.Integer, DB.ForeignKey("domain.id"), index=True)
+    description = DB.Column(DB.Text)
+    client_id = DB.Column(DB.Integer, unique=True, index=True)
+    is_active = DB.Column(DB.Boolean(), default=True)
+    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
+    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return "%s-%s-%s" % (self.client_id, self.is_active, self.domain_id)
+
+
+class DomainRole(DB.Model):
+    domain_id = DB.Column(
+        DB.Integer, DB.ForeignKey("domain.id"), primary_key=True
+    )
+    role_id = DB.Column(
+        DB.Integer, DB.ForeignKey("role.id"), primary_key=True
+    )
+    grant_implicitly = DB.Column(DB.Boolean(), default=False)
+    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
+    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return "%s-%s-%s" % (
+            self.domain_id, self.role_id, self.grant_implicitly
+        )
+
+
+class InvitationDomainRole(DB.Model):
+    invitation_id = DB.Column(
+        UUID, DB.ForeignKey("invitation.id"), primary_key=True
+    )
+    domain_id = DB.Column(
+        DB.Integer, primary_key=True
+    )
+    role_id = DB.Column(
+        DB.Integer, primary_key=True
+    )
+    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
+    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
+
+    __table_args__ = (
+        DB.ForeignKeyConstraint(
+            [domain_id, role_id],
+            [DomainRole.domain_id, DomainRole.role_id]
+        ), {}
+    )
+
+    def __repr__(self):
+        return "%s-%s" % (self.domain_id, self.role_id)
+
+
+class Invitation(DB.Model):
+    id = DB.Column(UUID, default=uuid.uuid1(), primary_key=True)
+    first_name = DB.Column(DB.Text)
+    last_name = DB.Column(DB.Text)
+    email = DB.Column(DB.VARCHAR(100), unique=True, index=True)
+    expires_at = DB.Column(DB.DateTime)
+    invitor_id = DB.Column(UUID, default=uuid.uuid1())
+    is_system_user = DB.Column(DB.Boolean(), default=False)
+    created_at = DB.Column(DB.DateTime, default=datetime.datetime.utcnow())
+    updated_at = DB.Column(DB.DateTime, onupdate=datetime.datetime.utcnow())
+
+    def __repr__(self):
+        return "%s-%s" % (self.email, self.expires_at)
