e43faa5e5d4185cefa24d86b865f752572456e6f
ruands <ruan.desousa@praekeltconsulting.com>
2018-02-19 18:00:52 +0200

    GE-614: Domain Hierarchy

---
+++ access_control/fixtures/load_domains.py
@@ -0,0 +1,174 @@
+# Domain hierachy based on:
+# https://praekelt.atlassian.net/wiki/spaces/GEM/pages/215515423/Roles+Permissions+Guidelines?preview=/55574535/118751390/Domain%20Hierarchy%20(5).png
+DOMAIN_HIERARCHY = {
+    "description": "The top level of the organisation",
+    "roles": {},
+    "subdomains": {
+        "Springster": {
+            "description": "Springster",
+            "roles": {},
+            "subdomains": {
+                "A Markets": {
+                    "description": "A markets",
+                    "roles": {},
+                    "subdomains": {
+                        "Philippines": {
+                            "description": "Philippines",
+                            "roles": {}
+                        },
+                        "South Africa": {
+                            "description": "South Africa",
+                            "roles": {}
+                        },
+                        "Nigeria": {
+                            "description": "Nigeria",
+                            "roles": {}
+                        }
+                    }
+                },
+                "C Markets": {
+                    "description": "C Markets",
+                    "roles": {},
+                    "subdomains": {
+                        "Algeria": {
+                            "description": "Algeria",
+                            "roles": {}
+                        },
+                        "Angola": {
+                            "description": "Angola",
+                            "roles": {}
+                        },
+                        "Benin": {
+                            "description": "Benin",
+                            "roles": {}
+                        },
+                        "Cape Verde": {
+                            "description": "Cape Verde",
+                            "roles": {}
+                        },
+                        "Chad": {
+                            "description": "Chad",
+                            "roles": {}
+                        },
+                        "Congo": {
+                            "description": "Democratic Republic of Congo",
+                            "roles": {}
+                        },
+                        "Gabon": {
+                            "description": "Gabon",
+                            "roles": {}
+                        },
+                        "Ghana": {
+                            "description": "Ghana",
+                            "roles": {}
+                        },
+                        "Guinea": {
+                            "description": "Guinea",
+                            "roles": {}
+                        },
+                    }
+                }
+            }
+        },
+        "GE Corporate": {
+            "description": "GE Corporate Site",
+            "roles": {},
+            "subdomains": {}
+        },
+        "Tega": {
+            "description": "TEGA",
+            "roles": {},
+            "subdomains": {
+                "Nigeria": {
+                    "description": "Nigeria",
+                    "roles": {},
+                },
+                "Malawi": {
+                    "description": "Malawi",
+                    "roles": {},
+                },
+                "Rwanda": {
+                    "description": "Rwanda",
+                    "roles": {},
+                },
+                "India": {
+                    "description": "India",
+                    "roles": {},
+                },
+                "USA": {
+                    "description": "United States of America",
+                    "roles": {},
+                },
+                "Bangladesh": {
+                    "description": "Bangladesh",
+                    "roles": {},
+                }
+            }
+        },
+        "Girl Connect": {
+            "description": "Girls Connect",
+            "roles": {},
+            "subdomains": {}
+        },
+        "Yegna": {
+            "description": "YEGNA",
+            "roles": {},
+            "subdomains": {
+                "Ethiopia": {
+                    "description": "Ethiopia",
+                    "roles": {},
+                    "subdomains": {}
+                }
+            }
+        },
+        "Zathu": {
+            "description": "ZATHU",
+            "roles": {},
+            "subdomains": {
+                "Malawi": {
+                    "description": "Malawi",
+                    "roles": {},
+                    "subdomains": {}
+                }
+            }
+        },
+        "India": {
+            "description": "Brand TBC",
+            "roles": {},
+            "subdomains": {
+                "India": {
+                    "description": "India",
+                    "roles": {},
+                    "subdomains": {}
+                }
+            }
+        },
+        "Ni Nyampinga": {
+            "description": "NI NYAMPINGA",
+            "roles": {},
+            "subdomains": {
+                "Rwanda": {
+                    "description": "Rwanda",
+                    "roles": {},
+                    "subdomains": {}
+                }
+            }
+        },
+        "GMP": {
+            "description": "Global Management Portal",
+            "roles": {},
+            "subdomains": {}
+        },
+        "Impact Dashboard": {
+            "description": "Impact Dashboard",
+            "roles": {},
+            "subdomains": {}
+        },
+        "Event Log": {
+            "description": "Event Log",
+            "roles": {},
+            "subdomains": {}
+        }
+    }
+}
+
