5f7eac1654fda7fbc775df166b8f222b21f960da
ruands <ruan.desousa@praekeltconsulting.com>
2018-02-20 16:22:59 +0200

    GE-614: Refactor script

--- seed_data.py
+++ seed_data.py
@@ -4,6 +4,7 @@ from flask import Flask
 from sqlalchemy import inspect
 from sqlalchemy.sql import ClauseElement
 
+from access_control.db_actions import get_or_create
 from access_control.fixtures.load_domains import DOMAIN_HIERARCHY
 from access_control.fixtures.load_permissions import PERMISSIONS
 from access_control.fixtures.load_resources import RESOURCES
@@ -22,40 +23,30 @@ class SeedDataLoader:
         self.load_roles()
         self.load_domain("Girl Effect Organisation", DOMAIN_HIERARCHY, None)
 
-    def get_or_create(self, model, **kwargs):
-        instance = DB.session.query(model).filter_by(**kwargs).first()
-        if instance:
-            return instance
-        else:
-            instance = model(**kwargs)
-            DB.session.add(instance)
-            DB.session.commit()
-            return instance
-
     def load_resources(self):
         print("Loading resources...")
         for resource in RESOURCES:
-            instance = self.get_or_create(Resource, urn=resource)
+            instance = get_or_create(Resource, urn=resource)
             print("Resource %s loaded..." % instance.urn)
         print("Done")
 
     def load_permissions(self):
         print("Loading permissions...")
         for permission in PERMISSIONS:
-            instance = self.get_or_create(Permission, name=permission)
+            instance = get_or_create(Permission, name=permission)
             print("Permission %s loaded..." % instance.name)
         print("Done")
 
     def load_roles(self):
         print("Loading roles...")
         for role in ROLES:
-            instance = self.get_or_create(Role, label=role)
+            instance = get_or_create(Role, label=role)
             print("Role %s loaded..." % instance.label)
         print("Done")
 
     def load_domain(self, name: str, detail: dict, parent: Domain):
         print("Loading domain %s..." % name)
-        domain = self.get_or_create(
+        domain = get_or_create(
             Domain, name=name,
             description=detail["description"],
             parent_id=parent
@@ -68,7 +59,8 @@ class SeedDataLoader:
         roles = detail.get("roles", {})
         for label, defaults in roles.items():
             role = Role.query.filter_by(label=label).first_or_404()
-            domain_role = self.get_or_create(DomainRole, domain=domain, role=role, defaults=defaults)
+            domain_role = get_or_create(
+                DomainRole, domain=domain, role=role, defaults=defaults)
 
         print("Done")
 
