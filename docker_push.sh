15f930f347818a04e1cd2e420e82ecf3d84860a0
ruands <ruan.desousa@praekeltconsulting.com>
2018-02-28 10:08:42 +0200

    GE-849: Put repo name in directly

--- docker_push.sh
+++ docker_push.sh
@@ -3,5 +3,5 @@
 echo $DOCKER_USERNAME
 echo $DOCKER_PASSWORD
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push girleffect/core-access-control
