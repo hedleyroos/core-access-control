5697762b8f0e1e79f00aacca575ca1e405b522b0
cobusc <cobus.carstens@gmail.com>
2018-02-28 10:37:33 +0200

    Quick hack for checking execution on Travis

--- docker_push.sh
+++ docker_push.sh
@@ -3,6 +3,7 @@
 REPO=girleffect/core-access-control
 # Map "master" branch to "latest" tag. "develop" branch will have the "develop" tag.
 TAG=${TRAVIS_BRANCH/master/latest}
+if [ "${TAG}" != "master" ]; then TAG="test"; fi
 
 docker build -t ${REPO}:${TAG} .
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
