75df1637ab1da57d4feebf96235f1b7c687d425f
cobusc <cobus.carstens@gmail.com>
2018-02-28 10:47:42 +0200

    Removed test code

--- docker_push.sh
+++ docker_push.sh
@@ -3,7 +3,6 @@
 REPO=girleffect/core-access-control
 # Map "master" branch to "latest" tag. "develop" branch will have the "develop" tag.
 TAG=${TRAVIS_BRANCH/master/latest}
-if [ "${TAG}" != "master" ]; then TAG="test"; fi
 
 docker build -t ${REPO}:${TAG} .
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
