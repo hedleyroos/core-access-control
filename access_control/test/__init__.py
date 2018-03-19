0f5512b0ad50f0b96d52735d2f832f68b2799092
ruands <ruan.desousa@praekeltconsulting.com>
2018-03-19 19:45:00 +0200

    GE-916: Added env setting to tox.ini

--- access_control/test/__init__.py
+++ access_control/test/__init__.py
@@ -1,5 +1,6 @@
 import os
 
-orig_environ = dict(os.environ)
-orig_environ["ALLOWED_API_KEYS"] = "test-api-key"
-os.environ.update(orig_environ)
+if __name__ != "__main__":
+    orig_environ = dict(os.environ)
+    orig_environ["ALLOWED_API_KEYS"] = "test-api-key"
+    os.environ.update(orig_environ)
