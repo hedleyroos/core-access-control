950528273fb41d892c6448da3a0eb6455edcfd20
AltusBarry <AltusBarry@users.noreply.github.com>
2018-03-28 14:17:17 +0200

    feature/GE-914: Remove extra data

--- access_control/fixtures/load_resources.py
+++ access_control/fixtures/load_resources.py
@@ -23,7 +23,6 @@ RESOURCES = [
     "urn:ge:user_data:sitedataschema",
     "urn:ge:user_data:usersitedata",
     "urn:ge:user_data:adminnote",
-    "urn:ge:user_data:extradata",
 
     "urn:ge:management_layer:refresh:all",
     "urn:ge:management_layer:refresh:domains",
