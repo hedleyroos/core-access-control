dc85fb73667801fdd65e7ee30604b779f5200da7
cobusc <cobus.carstens@gmail.com>
2018-03-30 15:17:10 +0200

    Enable persistent HTTP connections, which is very important for the management layer.

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 python manage.py db upgrade -d access_control/migrations
-# TODO: Disabled running uwsgi since it caused issues in the docker-compose
-# environment. Will re-enabled when I figure out how to let uwsgi keep connections
-# open.
-# uwsgi uwsgi.ini
-python3 -m swagger_server
+uwsgi uwsgi.ini
