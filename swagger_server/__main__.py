111041e8989ca950e9f7b95f6f673d652a817136
AltusBarry <AltusBarry@users.noreply.github.com>
2018-04-06 13:28:26 +0200

    feature/GE-953: Remove example code

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,16 +4,13 @@ import connexion
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
-from ge_core_shared import exception_handlers, middleware
+from ge_core_shared import exception_handlers, middleware, handlers
 
 import project.app
 from swagger_server import encoder
 
-def response_handler(response):
-    response.headers["X-Total-Count"] = 100
-    return response
-
 DB = SQLAlchemy()
+
 
 # We create and set up the app variable in the global context as it is used by uwsgi.
 app = connexion.App(__name__, specification_dir='./swagger/')
@@ -23,7 +20,7 @@ app.app.config = project.app.APP.config
 DB.init_app(app.app)
 app.add_error_handler(SQLAlchemyError, exception_handlers.db_exceptions)
 app.app.wsgi_app = middleware.AuthMiddleware(app.app.wsgi_app)
-app.app.after_request(response_handler)
+app.app.after_request(handlers.add_total_count)
 
 if __name__ == '__main__':
     app.run(port=8080)
