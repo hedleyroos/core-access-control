f374b4635fe5778aae9ea6512ccdccdff882ee2a
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-06-05 11:21:19 +0200

    Added Roles to load_domains.py

--- access_control/fixtures/load_domains.py
+++ access_control/fixtures/load_domains.py
@@ -1,72 +1,99 @@
 # Domain hierarchy based on:
 # https://praekelt.atlassian.net/wiki/spaces/GEM/pages/215515423/Roles+Permissions+Guidelines?preview=/55574535/118751390/Domain%20Hierarchy%20(5).png
+ROLES = [
+    "role_delegator",
+    "governance_admin",
+    "governance_viewer",
+    "data_admin",
+    "data_editor",
+    "data_viewer",
+    "content_admin",
+    "content_editor",
+    "content_viewer"
+]
+
 DOMAIN_HIERARCHY = [
     {
         "name": "girl_effect_organisation",
         "description": "The top level of the organisation",
-        "roles": {},
+        "roles": ["tech_admin"],
         "subdomains": [
             {
                 "name": "springster",
                 "description": "springster",
+                "roles": ROLES,
                 "subdomains": [
                     {
                         "name": "a_markets",
                         "description": "A Markets",
+                        "roles": ROLES,
                         "subdomains": [
                             {
                                 "name": "philippines",
-                                "description": "Philippines"
+                                "description": "Philippines",
+                                "roles": ROLES
                             },
                             {
                                 "name": "south_africa",
-                                "description": "South Africa"
+                                "description": "South Africa",
+                                "roles": ROLES
                             },
                             {
                                 "name": "nigeria",
-                                "description": "Nigeria"
+                                "description": "Nigeria",
+                                "roles": ROLES
                             }
                         ]
                     },
                     {
                         "name": "c_markets",
                         "description": "C Markets",
+                        "roles": ROLES,
                         "subdomains": [
                             {
                                 "name": "algeria",
-                                "description": "Algeria"
+                                "description": "Algeria",
+                                "roles": ROLES
                             },
                             {
                                 "name": "angola",
-                                "description": "Angola"
+                                "description": "Angola",
+                                "roles": ROLES
                             },
                             {
                                 "name": "benin",
-                                "description": "Benin"
+                                "description": "Benin",
+                                "roles": ROLES
                             },
                             {
                                 "name": "cape_verde",
-                                "description": "Cape Verde"
+                                "description": "Cape Verde",
+                                "roles": ROLES
                             },
                             {
                                 "name": "chad",
-                                "description": "Chad"
+                                "description": "Chad",
+                                "roles": ROLES
                             },
                             {
                                 "name": "congo",
-                                "description": "DRC"
+                                "description": "DRC",
+                                "roles": ROLES
                             },
                             {
                                 "name": "gabon",
-                                "description": "Gabon"
+                                "description": "Gabon",
+                                "roles": ROLES
                             },
                             {
                                 "name": "ghana",
-                                "description": "Ghana"
+                                "description": "Ghana",
+                                "roles": ROLES
                             },
                             {
                                 "name": "guinea",
-                                "description": "Guinea"
+                                "description": "Guinea",
+                                "roles": ROLES
                             },
                         ]
                     }
@@ -152,7 +179,7 @@ DOMAIN_HIERARCHY = [
             },
             {
                 "name": "gmp",
-                "description": "Global Management Portal",
+                "description": "Global Management Portal"
             },
             {
                 "name": "impact_dash",
